b148adca63e2c485dff0c918fef27f9ebd22e684
lujain415 <lujainalaamri55@gmail.com>
2025-06-22 21:37:36 +0300

    completed

---
+++ main.py
@@ -0,0 +1,11 @@
+def additoin(x, y):
+  try:  
+     x = 10
+     y = 20
+     
+     print("Addition:", x + b)
+     print("the operation is successful")
+  except NameError:
+         print("The variable 'b' is not defined")
+
+additoin(x=1,y=2)
